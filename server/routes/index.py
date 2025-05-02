195eb2f04f2399bd504b6948d4dbfc31ef6739df
AsumVictor <iamasum369@gmail.com>
2025-05-02 14:17:58 +0000

    Fix names

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 
 # Import sub-routes
-from routes.load import load_bp
+from routes.zone import zone_bp
 
 index_bp = Blueprint('index_bp', __name__)
 
 # All routes should be here
-index_bp.register_blueprint(load_bp, url_prefix='/load')
+index_bp.register_blueprint(zone_bp, url_prefix='/zone')
 
 @index_bp.route('/')
 def hello():
